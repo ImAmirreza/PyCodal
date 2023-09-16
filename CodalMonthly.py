a6c66d5e947288537f69cd14391795d0d49b3510
Amirreza <Amirreza.80.sadeghi@gmail.com>
2023-09-16 21:27:42 +0330

    removing symbol var in raw_link_extractor body

--- CodalMonthly.py
+++ CodalMonthly.py
@@ -10,7 +10,6 @@ def raw_link_extractor(symbol:str,only_first_page:bool = 0,number:int=50)-> list
     page = 1
     total_data  = []
     while len(total_data) < number:
-        symbol = "کگهر"
         URL = f"https://search.codal.ir/api/search/v2/q?&Audited=true&AuditorRef=-1&Category=3&Childs=false&CompanyState=0&CompanyType=1&Consolidatable=true&IsNotAudited=false&Isic=132004&Length=-1&LetterType=58&Mains=true&NotAudited=true&NotConsolidatable=true&PageNumber={page}&Publisher=false&Symbol={symbol}&TracingNo=-1&search=true"
         data = requests.get(URL, headers=headers).json()
         total_data += data["Letters"] 
