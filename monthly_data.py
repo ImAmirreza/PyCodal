585b700270d4cf446f4cefeb14234f07b8b40c57
Amirreza Sadeghi <Amirreza.80.sadeghi@gmail.com>
2025-01-17 15:52:43 +0330

    refactor: moving get_html and get_json to utils

--- monthly_data.py
+++ monthly_data.py
@@ -2,28 +2,7 @@ import re
 import json
 import requests
 from setting import BASE_URL, MAIN_HEADERS
-
-def get_html(url):
-    """Makes a GET request to the specified URL and returns the HTML response."""
-    return requests.get(url, headers=MAIN_HEADERS).text
-
-def get_json(url):
-    """Extracts a JSON string from the HTML response using a regular expression and parses it into a Python object."""
-    html_source = get_html(url)
-    pattern = r"var datasource = (.*?)</script>"
-    match = re.search(pattern, html_source, re.DOTALL)
-    if match:
-        json_string = match.group(1).strip().replace(";","")
-        try:
-            data = json.loads(json_string)
-        except json.JSONDecodeError as e:
-            print(f"Failed to parse JSON: {e}")
-            print(json_string)
-            return None
-    else:
-        print("No match found.")
-        return None
-    return data
+from utils import get_json
 
 def get_monthly_data(url, symbol):
     """Downloads a JSON file from the specified URL and saves it to a file in the specified directory."""
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import requests
+import json
+from setting import MAIN_HEADERS
+import re
 def arabic_to_english(arabic_num):
     mapping = {
         '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
@@ -5,3 +9,24 @@ def arabic_to_english(arabic_num):
     }
     return ''.join(mapping[c] for c in arabic_num)
 
+def get_html(url):
+    """Makes a GET request to the specified URL and returns the HTML response."""
+    return requests.get(url, headers=MAIN_HEADERS).text
+
+def get_json(url):
+    """Extracts a JSON string from the HTML response using a regular expression and parses it into a Python object."""
+    html_source = get_html(url)
+    pattern = r"var datasource = (.*?)</script>"
+    match = re.search(pattern, html_source, re.DOTALL)
+    if match:
+        json_string = match.group(1).strip().replace(";","")
+        try:
+            data = json.loads(json_string)
+        except json.JSONDecodeError as e:
+            print(f"Failed to parse JSON: {e}")
+            print(json_string)
+            return None
+    else:
+        print("No match found.")
+        return None
+    return data
